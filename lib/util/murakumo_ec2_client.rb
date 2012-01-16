504e92c3fd3b472af26beee721094f860e98c1a4
Genki Sugawara <sugawara@cookpad.com>
2012-01-16 18:46:10 +0900

    update api version

--- lib/util/murakumo_ec2_client.rb
+++ lib/util/murakumo_ec2_client.rb
@@ -11,10 +11,11 @@ module Murakumo
 
     class EC2Client
 
-      API_VERSION = '2011-12-01'
+      API_VERSION = '2011-12-15'
       SIGNATURE_VERSION = 2
+      SIGNATURE_ALGORITHM = :SHA256
 
-      def initialize(accessKeyId, secretAccessKey, endpoint = nil, algorithm = :SHA256)
+      def initialize(accessKeyId, secretAccessKey, endpoint = nil)
         unless endpoint
           local_hostname = Net::HTTP.get('169.254.169.254', '/latest/meta-data/local-hostname')
 
@@ -28,7 +29,6 @@ module Murakumo
         @accessKeyId = accessKeyId
         @secretAccessKey = secretAccessKey
         @endpoint = endpoint
-        @algorithm = algorithm
 
         if /\A[^.]+\Z/ =~ @endpoint
           @endpoint = "ec2.#{@endpoint}.amazonaws.com"
@@ -41,7 +41,7 @@ module Murakumo
           :Version          => API_VERSION,
           :Timestamp        => Time.now.getutc.strftime('%Y-%m-%dT%H:%M:%SZ'),
           :SignatureVersion => SIGNATURE_VERSION,
-          :SignatureMethod  => "Hmac#{@algorithm}",
+          :SignatureMethod  => "Hmac#{SIGNATURE_ALGORITHM}",
           :AWSAccessKeyId   => @accessKeyId,
         }.merge(params)
 
@@ -69,7 +69,7 @@ module Murakumo
       def aws_sign(params)
         params = params.sort_by {|a, b| a.to_s }.map {|k, v| "#{CGI.escape(k.to_s)}=#{CGI.escape(v.to_s)}" }.join('&')
         string_to_sign = "POST\n#{@endpoint}\n/\n#{params}"
-        digest = OpenSSL::HMAC.digest(OpenSSL::Digest.const_get(@algorithm).new, @secretAccessKey, string_to_sign)
+        digest = OpenSSL::HMAC.digest(OpenSSL::Digest.const_get(SIGNATURE_ALGORITHM).new, @secretAccessKey, string_to_sign)
         Base64.encode64(digest).gsub("\n", '')
       end
     end
